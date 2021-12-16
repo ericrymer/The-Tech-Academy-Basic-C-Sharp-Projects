67d77fbaefbcd07069e5c35f3a1426031fbec337
ericrymer <90289727+ericrymer@users.noreply.github.com>
2021-12-16 15:47:00 -0800

    Add files via upload

---
+++ CallingMethods/CallingMethods/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class MathOperations
+    {
+        public MathOperations()
+        {
+            secondnumber = 10;
+        }
+        public int secondnumber { get; set; }
+
+        //Method to Add 
+        public int Addition(int numEntered)
+        {
+            return (secondnumber + numEntered);
+        }
+
+        //Method to Subtract
+        public int Subtraction(int numEntered)
+        {
+            //return the result of the mathematical operation
+            return (secondnumber - numEntered);
+        }
+        //Method to Multiply
+        public int Multiplication(int numEntered)
+        {
+            //return the result of the mathematical operation
+            return (secondnumber * numEntered);
+        }
+        //Method to Divide
+        public int Division(int numEntered)
+        {
+            //return the result of the mathematical operation
+            return (secondnumber / numEntered);
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            //Requests user to enter a number
+            Console.WriteLine("Please enter a number");
+            //Data is stored in the variable number
+            int number = int.Parse(Console.ReadLine());
+            //Class Operations 
+            MathOperations mathOperations = new MathOperations();
+            //"Addition"
+            Console.WriteLine("The number " + mathOperations.secondnumber + " + " + number + " is equal to " + mathOperations.Addition(number));
+            //Subtraction
+            Console.WriteLine("The number " + mathOperations.secondnumber + " - " + number + " is equal to " + mathOperations.Subtraction(number));
+            //Multiplication
+            Console.WriteLine("The number " + mathOperations.secondnumber + " * " + number + " is equal to " + mathOperations.Multiplication(number));
+            //Division
+            Console.WriteLine("The number " + mathOperations.secondnumber + " / " + number + " is equal to " + mathOperations.Division(number));
+        }
+            //If there is any exception, catch that exception
+            catch (Exception e)
+            {
+                //print a message error with the exception on the console
+                Console.WriteLine("Error !!!! -> " + e.Message);
+            }
+            finally
+            {
+                //wait until user press enter key
+                Console.ReadLine();
+            }
+        }
+    }
+}
