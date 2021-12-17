9cd28ebf2142fdcf226566db38aa4e97557af907
ericrymer <90289727+ericrymer@users.noreply.github.com>
2021-12-17 15:35:27 -0800

    Step 279 Assignment

---
+++ MainMethod/MainMethod/MathOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class MathOperation
+    {
+        public MathOperation()
+        {
+            auxNumber = 5;
+        }
+
+        public int auxNumber { get; set; }
+
+        public int Addition(int wholeNumber)
+        {
+            return wholeNumber + auxNumber;
+        }
+
+        //Addition Method with decimal parameter
+        public int Addition(decimal decimalNumber)
+        {
+            return Convert.ToInt32(decimalNumber) + auxNumber;
+        }
+
+        //Addition Method with String parameter
+        public int Addition(string stringNumber)
+        {
+            //if error, return to main.
+            return Convert.ToInt32(stringNumber) + auxNumber;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Class MathOperation is instantiated
+                MathOperation mathOperation = new MathOperation();
+                //Three variables were used for the example
+                int numberA = 25;
+                decimal numberB = 22.5678m;
+                string numberC = "47";
+                //First Method with an integer parameter
+                Console.WriteLine(mathOperation.auxNumber + " + " + numberA + " = " + mathOperation.Addition(numberA));
+                //Second Method with a decimal parameter
+                Console.WriteLine(mathOperation.auxNumber + " + " + numberB + " = " + mathOperation.Addition(numberB));
+                //Third method with a string parameter
+                Console.WriteLine(mathOperation.auxNumber + " + " + numberC + " = " + mathOperation.Addition(numberC));
+            }
+            //Catch any exceptions if errors come up
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!!!! " + e.Message);
+            }
+            finally
+            {
+                //Wait until the user presses the enter key
+                Console.ReadLine();
+            }
+        }
+    }
+}
