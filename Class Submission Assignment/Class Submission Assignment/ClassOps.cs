9b5433b04f45bcaa5b6f7c094f136837a777a77a
ericrymer <90289727+ericrymer@users.noreply.github.com>
2021-12-17 16:15:07 -0800

    Step 285 Class Submission

---
+++ Class Submission Assignment/Class Submission Assignment/ClassOps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+    class ClassOps
+    {
+        public void Divide(int numA, out int numB)
+        {
+            if ((numA % 2)== 0)
+            {
+                numB = numA / 2;
+            }
+            else
+            {
+                numB = -1;
+            }
+        }
+        public void Divide(string numA, out int numB)
+        {
+            Divide(Convert.ToInt32(numA), out numB);
+        }
+    }
+}
---
+++ Class Submission Assignment/Class Submission Assignment/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // class ClassOps is instantiated
+                ClassOps classOps = new ClassOps();
+
+                //Ask user to enter a number
+                Console.WriteLine("Please enter a whole number");
+
+                //int number
+                string number = Console.ReadLine();
+                //For the class ClassOps 
+                int numB = 0; 
+                //For the class ClassOpsStatic
+                int numC = 0;
+                //Stores result in the variable "numB"
+                classOps.Divide(number, out numB);
+
+                ClassOpsStatic.Divide(number, out numC);
+
+                Console.WriteLine("\nResult of the class \"ClassOps\"");
+                if (numB == -1)
+                {
+                    //Will be shown if number cannot be divided by 2.
+                    Console.WriteLine("The number " + number + " cannot be divided by 2");
+                }
+                else
+                {
+                    //If sucessful print below
+                    Console.WriteLine("The number " + number + " divided by 2 is equal to " + numB);
+                }
+
+                
+                Console.WriteLine("\nResult of the static class \"ClassOpsStatic\"");
+                if (numC == -1)
+                {
+                    //Will be shown if number cannot be divided by 2.
+                    Console.WriteLine("The number " + number + " cannot be divided by 2");
+                }
+                else
+                {
+                    //If sucessful print below
+                    Console.WriteLine("The number " + number + " divided by 2 is equal to " + numC);
+                }
+            }
+            //If exist an exception, is catched and print the error message on the console
+            catch (Exception e)
+            {
+                Console.WriteLine("Error -> " + e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+                
+
+                
