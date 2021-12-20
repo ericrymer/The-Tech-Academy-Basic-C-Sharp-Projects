b170e1f1da94084904ad965c79a850ccd3678357
ericrymer <90289727+ericrymer@users.noreply.github.com>
2021-12-20 15:20:50 -0800

    Method Submission Page 280

---
+++ Method Submission/Method Submission/MathOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission
+{
+    //MathOperation class is created
+    class MathOperation
+    {
+        public int Multiplication(int numA, int numB = 4)
+        {
+            //Return result of numA multiplied by numB
+            return numA * numB;
+        }
+
+    }
+}
---
+++ Method Submission/Method Submission/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //MathOperation class is instantiated
+                MathOperation mathOperation = new MathOperation();
+                //User is asked to enter a whole number
+                Console.WriteLine("Please, enter a whole number");
+                //Data entered will be stored in variable "numA"
+                int numA = Convert.ToInt32(Console.ReadLine());
+                //User is asked to enter a second number
+                Console.WriteLine("Please enter a 2nd whole number, if you don't want to enter a number, press the enter key");
+                //Data entered is stored within the variable "auxNumberB"
+                string auxNumberB = Console.ReadLine();
+                //The variable "numB" will store the valid number typed by the user
+                int numB;
+                //Try parising the value of auxNumberB to int
+                if (!int.TryParse(auxNumberB, out numB))
+                {
+                    if(auxNumberB.Length > 0)
+                    {
+                        throw new FormatException("If you don\'t want to write the second whole number, please press the enter key;" +
+                            " you entered another data that cannot be converted to integer");
+                    }
+                    else
+                    {
+                       Console.WriteLine("\nThe mathematical operation " + numA + " * 4 = " + mathOperation.Multiplication(numA));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nThe mathematical operation " + numA + " * " + numB + " = " + mathOperation.Multiplication(numA, numB));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nError!!!! -> " + e.Message);
+            }
+            finally
+            {
+                //Wait until the user press the enter key
+                Console.ReadLine();
+            }
+        }
+    }
+}
