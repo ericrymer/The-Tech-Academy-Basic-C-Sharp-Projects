a71a72b9714b617e88fb1decf759176794e52f6c
ericrymer <90289727+ericrymer@users.noreply.github.com>
2021-12-20 15:36:52 -0800

    Method Class Step 282

---
+++ Method Class Submission/Method Class Submission/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Submission
+{
+    //MathOperation class is created
+    class MathOperation
+    {
+        //Multiplication method is created
+        //Method will have two parameters, 2nd parameter will store the result of the math operation numA * 5
+        public void Multiplication(int numA, int numB)
+        {
+            int auxNumber = numA * 5;
+            Console.WriteLine(numB);
+        }
+    }
+}
---
+++ Method Class Submission/Method Class Submission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Method_Class_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MathOperation class is instantiated
+            MathOperation mathOperation = new MathOperation();
+            //For purpose of this exercise, the variables numA and numB are created
+            int numA = 15, numB = 3;
+            //Call the method in the class, passing in two numbers
+            mathOperation.Multiplication(56, 23);
+            //Call the method "Multiplication" with the two parameters, specifying the parameters by name
+            mathOperation.Multiplication(numA: numA, numB: numB);
+            Console.ReadLine();
+        }
+    }
+}
